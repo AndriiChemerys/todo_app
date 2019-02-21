bea1a7f5ac1ab5c25b9d31b84d12b610346d2a55
AndriiChemerys <andriichemerys@gmail.com>
2019-02-21 20:19:13 +0100

    adding search engine

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
+// toDo variables
+
 const toDoArray = [];
 const form=document.querySelector('form');
 const ul=document.querySelector('ul');
 const taskCount=document.querySelector('h1 span');
 const listItems=document.getElementsByClassName('task');
 const input=document.querySelector('.add-task');
+
+
 
 // removeTask - function that removes li item from list and task item from array, also counting tasks and create new list
 
@@ -33,6 +37,8 @@ const addTask=(e)=>{
     task.querySelector('button').addEventListener('click', removeTask);
 }
 
+//creating a new list
+
 const renderList=()=>{
     ul.textContent="";
     toDoArray.forEach((toDoElement, key)=>{
@@ -42,3 +48,23 @@ const renderList=()=>{
 }
 
 form.addEventListener('submit', addTask)
+
+// search engine
+
+// ################## [Theres a bug, search is not working correctly! Need to be fixed!] ###########
+
+const searchInput=document.querySelector('.search')
+
+const searchTask= (e) =>{
+    const liElements=document.querySelectorAll('li');
+    console.log(e.target.value);
+    const searchText = e.target.value.toLowerCase();
+    let tasks=[...liElements]; //creating array from nodeList
+    console.log(tasks);
+    tasks=tasks.filter(li=>li.textContent.toLowerCase().includes(searchText)) //cheking and filtering list
+    console.log(tasks);
+    ul.textContent="";
+    tasks.forEach(li=>ul.appendChild(li))
+}
+
+searchInput.addEventListener('input', searchTask)
